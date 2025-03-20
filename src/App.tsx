c7392adbc407dbedc2a409cb081360bc0beb94dd
Mduduzi <leonard.m.mataboge@gmail.com>
2025-03-20 08:46:20 +0200

    Deployed to Vercel

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AI Todo List</h1>
+        <p>Hello from Grok 3 and xAI! Vulnerabilities are gone!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
